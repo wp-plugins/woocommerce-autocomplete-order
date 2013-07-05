564678778ede48c391500cf26ff7e115c7b9f15e
Rashef <Rashef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-05 06:43:50 +0000

    Tagging 0.1.1
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-autocomplete-order/tags/0.1.1@736552 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ woocommerce-autocomplete-order.php
@@ -0,0 +1,20 @@
+<?php
+/*
+	Plugin Name: WooCommerce - Autocomplete Order
+	Plugin URI: 
+	Description: This plugin allows to automatically set any order for any virtual product on Completed status right after a successfull payment.
+	Version: 0.1.1
+	Author: Mirko Grewing
+	Author URI: http://www.mirkogrewing.eu	
+		
+		Copyright: © 2013 Mirko Grewing (email : mirko@grewing.us)	
+		License: GNU General Public License v3.0	
+		License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+define('WOOAO_DIR', plugin_dir_path(__FILE__));
+define('WOOAO_URL', plugin_dir_url(__FILE__));
+function wooao_load(){
+    require_once(WOOAO_DIR.'includes/core.php');}
+wooao_load();
+?>
