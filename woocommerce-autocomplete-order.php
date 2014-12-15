dd89fbb08b3bc9b1528a49dc2dc90f1923fb85aa
Rashef <Rashef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-15 22:55:58 +0000

    Right code for the update
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-autocomplete-order/tags/1.0@1045323 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-autocomplete-order.php
+++ woocommerce-autocomplete-order.php
@@ -3,11 +3,11 @@
 	Plugin Name: WooCommerce - Autocomplete Order
 	Plugin URI: 
 	Description: Do you hate WooCommerce for obliging you to manually approve every order placed for non-downloadable goods? This plugin is the answer, since allows to automatically mark orders for **virtual** products as Completed after a successful payment (e.g. with PayPal or Credit Card).
-	Version: 1.0
+	Version: 1.1
 	Author: Mirko Grewing
 	Author URI: http://www.mirkogrewing.it	
 		
-		Copyright: Â© 2013 Mirko Grewing (email : mirko.grewing@live.com)	
+		Copyright: © 2013 Mirko Grewing (email : mirko.grewing@live.com)	
 		License: GNU General Public License v3.0	
 		License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -24,7 +24,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
          * @author    Mirko Grewing <mirko@grewing.co.uk>
          * @copyright 2014 Mirko Grewing
          * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
-         * @version   1.0
+         * @version   1.1
          * @link      http://www.nitelab.org/
          * @since     Class available since Release 0.1
          *
@@ -44,22 +44,45 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
              */
             function __construct()
             {
-                //add settings tab
-                add_filter('woocommerce_settings_tabs_array', array($this,'woocommerce_settings_tabs_array'), 50);
-                //show settings tab
-                add_action('woocommerce_settings_tabs_'.$this->id, array($this,'show_settings_tab'));
-                //save settings tab 
-                add_action('woocommerce_update_options_'.$this->id, array($this,'update_settings_tab'));
-                //add tabs select field
-                add_action('woocommerce_admin_field_'.$this->post_type,array($this, 'show_'.$this->post_type.'_field'), 10);
-                //save tabs select field
-                add_action('woocommerce_update_option_'.$this->post_type,array($this, 'save_'.$this->post_type.'_field'), 10);
+				if (is_admin()) {
+					//add settings tab
+					add_filter('woocommerce_settings_tabs_array', array($this,'woocommerce_settings_tabs_array'), 50);
+					//show settings tab
+					add_action('woocommerce_settings_tabs_'.$this->id, array($this,'show_settings_tab'));
+					//save settings tab 
+					add_action('woocommerce_update_options_'.$this->id, array($this,'update_settings_tab'));
+					//add tabs select field
+					add_action('woocommerce_admin_field_'.$this->id,array($this, 'show_'.$this->id.'_field'), 10);
+					//save tabs select field
+					add_action('woocommerce_update_option_'.$this->id,array($this, 'save_'.$this->id.'_field'), 10);
+				}
                 //ajax search handler
                 //add_action('wp_ajax_woocommerce_json_custom_tabs', array($this,'woocommerce_json_custom_tabs'));
                 //register_post_type
                 add_action('init', array($this,'autocompleteOrders'), 0);
-            }
-
+				
+				function mgWooeoNotice()
+				{
+					global $current_user;
+					$user_id = $current_user->ID;
+					if (!get_user_meta($user_id, 'mg_ignore_notice')) {
+						echo '<div class="updated"><p>';
+						printf(__('This is an annoying nag message. | <a href="%1$s">Hide Notice</a>'), '?mgWooeoNoticeIgnore=0');
+						echo "</p></div>";
+					}
+				}
+				
+				function mgWooeoNoticeIgnore() {
+					global $current_user;
+					$user_id = $current_user->ID;
+					if (isset($_GET['mgWooeoNoticeIgnore']) && '0' == $_GET['mgWooeoNoticeIgnore']) {
+						add_user_meta($user_id, 'mg_ignore_notice', 'true', true);
+					}
+				}
+				//add_action('admin_notices', 'mgWooeoNotice');
+				//add_action('admin_init', 'mgWooeoNoticeIgnore');
+			}
+			
 			/**
 			 * woocommerce_settings_tabs_array 
 			 * Used to add a WooCommerce settings tab
@@ -103,9 +126,9 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 						'id'       => 'wc_'.$this->id.'_section_title'
 					),
 					'title' => array(
-						'name'     => __( 'Mode', 'wooExtraOptions' ),
+						'name'     => __('Mode', 'wooExtraOptions'),
 						'type'     => 'select',
-						'desc'     => __( 'Select the type of autocompletion you want to activate.', 'wooExtraOptions' ),
+						'desc'     => __('Select the type of autocompletion you want to activate.', 'wooExtraOptions'),
 						'desc_tip' => true,
 						'default'  => 'off',
 						'id'       => 'wc_'.$this->id.'_mode',
@@ -149,7 +172,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 						$order->update_status('completed');
 					}
 				} elseif ($mode == 'paid') {
-					add_filter( 'woocommerce_payment_complete_order_status', 'autocompletePaidOrders', 10, 2 );
+					add_filter('woocommerce_payment_complete_order_status', 'autocompletePaidOrders', 10, 2);
 					/**
 					 * autocompletePaidOrders 
 					 * Register custom tabs Post Type
@@ -164,7 +187,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 						return $order_status;
 					}
 				} elseif ($mode == 'virtual') {
-					add_filter( 'woocommerce_payment_complete_order_status', 'autocompleteVirtualOrders', 10, 2 );
+					add_filter('woocommerce_payment_complete_order_status', 'autocompleteVirtualOrders', 10, 2);
 					/**
 					 * autocompleteVirtualOrders 
 					 * Register custom tabs Post Type
@@ -199,6 +222,8 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 		}//end MG_Woo_Extra_Options class.
 		new MG_Woo_Extra_Options();
 	}
+} elseif (defined('WOOCOMMERCE_VERSION') && version_compare(WOOCOMMERCE_VERSION, '2.1', '<')) {
+	wc_add_notice(sprintf(__("This plugin requires WooCommerce 2.1 or higher!", "wooExtraOptions" ), 'error'));
 } else {
     /**
      * Check if WooCommerce is up and running
